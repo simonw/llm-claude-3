917168631429fbd4b8865d3ea6bead3ff49e1ac3
Simon Willison <swillison@gmail.com>
2024-03-04 07:38:39 -0800

    Added first test, using pytest-recording - refs #1

--- tests/test_claude_3.py
+++ tests/test_claude_3.py
@@ -1,6 +1,20 @@
-from llm.plugins import pm
+import llm
+import pytest
 
 
-def test_plugin_is_installed():
-    names = [mod.__name__ for mod in pm.get_plugins()]
-    assert "llm_claude_3" in names
+@pytest.mark.vcr
+def test_prompt():
+    model = llm.get_model("claude-3-opus")
+    model.key = "sk-..."
+    response = model.prompt("Two names for a pet pelican, be brief")
+    assert str(response) == "1. Pelly\n2. Beaky"
+    assert response.response_json == {
+        "id": "msg_01QPXzRdFQ5sibaQezm3b8Dz",
+        "content": [{"text": "1. Pelly\n2. Beaky", "type": "text"}],
+        "model": "claude-3-opus-20240229",
+        "role": "assistant",
+        "stop_reason": "end_turn",
+        "stop_sequence": None,
+        "type": "message",
+        "usage": {"input_tokens": 17, "output_tokens": 1},
+    }
