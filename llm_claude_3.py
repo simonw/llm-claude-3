106926d4f0a56aecf6f57285a93f256e3ae09c49
jvmncs <jvmncs@gmail.com>
2024-03-05 17:13:45 -0500

    add sampling options ClaudeMessages.Options

--- llm_claude_3.py
+++ llm_claude_3.py
@@ -1,6 +1,6 @@
 from anthropic import Anthropic
 import llm
-from pydantic import Field, field_validator
+from pydantic import Field, field_validator, model_validator
 from typing import Optional, List
 
 
@@ -11,20 +11,74 @@ def register_models(register):
     register(ClaudeMessages("claude-3-sonnet-20240229"), aliases=("claude-3-sonnet",))
 
 
+class ClaudeOptions(llm.Options):
+    max_tokens: Optional[int] = Field(
+        description="The maximum number of tokens to generate before stopping",
+        default=4_096,
+    )
+
+    temperature: Optional[float] = Field(
+        description="Amount of randomness injected into the response. Defaults to 1.0. Ranges from 0.0 to 1.0. Use temperature closer to 0.0 for analytical / multiple choice, and closer to 1.0 for creative and generative tasks. Note that even with temperature of 0.0, the results will not be fully deterministic.",
+        default=1.0,
+    )
+
+    top_p: Optional[float] = Field(
+        description="Use nucleus sampling. In nucleus sampling, we compute the cumulative distribution over all the options for each subsequent token in decreasing probability order and cut it off once it reaches a particular probability specified by top_p. You should either alter temperature or top_p, but not both. Recommended for advanced use cases only. You usually only need to use temperature.",
+        default=None,
+    )
+
+    top_k: Optional[int] = Field(
+        description="Only sample from the top K options for each subsequent token. Used to remove 'long tail' low probability responses. Recommended for advanced use cases only. You usually only need to use temperature.",
+        default=None,
+    )
+
+    user_id: Optional[str] = Field(
+        description="An external identifier for the user who is associated with the request",
+        default=None,
+    )
+
+    @field_validator("max_tokens")
+    @classmethod
+    def validate_max_tokens(cls, max_tokens):
+        if not (0 < max_tokens <= 4_096):
+            raise ValueError("max_tokens must be in range 1-4,096")
+        return max_tokens
+
+    @field_validator("temperature")
+    @classmethod
+    def validate_temperature(cls, temperature):
+        if not (0.0 <= temperature <= 1.0):
+            raise ValueError("temperature must be in range 0.0-1.0")
+        return temperature
+
+    @field_validator("top_p")
+    @classmethod
+    def validate_top_p(cls, top_p):
+        if top_p is not None and not (0.0 <= top_p <= 1.0):
+            raise ValueError("top_p must be in range 0.0-1.0")
+        return top_p
+
+    @field_validator("top_k")
+    @classmethod
+    def validate_top_k(cls, top_k):
+        if top_k is not None and top_k <= 0:
+            raise ValueError("top_k must be a positive integer")
+        return top_k
+
+    @model_validator(mode="after")
+    def validate_temperature_top_p(self):
+        if self.temperature != 1.0 and self.top_p is not None:
+            raise ValueError("Only one of temperature and top_p can be set")
+        return self
+
+
 class ClaudeMessages(llm.Model):
     needs_key = "claude"
     key_env_var = "ANTHROPIC_API_KEY"
     can_stream = True
 
-    class Options(llm.Options):
-        max_tokens: int = Field(
-            description="The maximum number of tokens to generate before stopping",
-            default=4096,
-        )
-        user_id: Optional[str] = Field(
-            description="An external identifier for the user who is associated with the request",
-            default=None,
-        )
+    class Options(ClaudeOptions):
+        ...
 
     def __init__(self, model_id):
         self.model_id = model_id
@@ -55,6 +109,14 @@ class ClaudeMessages(llm.Model):
         }
         if prompt.options.user_id:
             kwargs["metadata"] = {"user_id": prompt.options.user_id}
+
+        if prompt.options.top_p:
+            kwargs["top_p"] = prompt.options.top_p
+        else:
+            kwargs["temperature"] = prompt.options.temperature
+
+        if prompt.options.top_k:
+            kwargs["top_k"] = prompt.options.top_k
 
         if prompt.system:
             kwargs["system"] = prompt.system
